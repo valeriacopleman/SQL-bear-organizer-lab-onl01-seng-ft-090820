1d8688a0b8a242cce565109af101202fb5f016e4
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-06 00:05:40 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,11 +19,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(*) "
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(*) DESC LIMIT 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT temperaments, COUNT(temperaments) FROM "
 end
 
 def selects_bear_that_killed_Tim
